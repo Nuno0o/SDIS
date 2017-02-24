637f29d97f16f3d50298b4bbfdda6a3ee2367f9f
gribeirofeup <goncalo.ribeiro@fe.up.pt>
2017-02-24 11:23:35 +0000

    ON UR KNEES

---
+++ TP2/Server.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	public static void main(String [ ] args) throws Exception{
+
+		//Port Number
+		Integer port1 = Integer.parseInt(args[0]);
+		
+		//Mcast addr
+		String mcast = args[1];
+		
+		//Mcast port
+		Integer portmcast = Integer.parseInt(args[2]);
+		
+		//Start thread
+		new ServerThread(port1,mcast,portmcast).start();
+	}
+}
---
+++ TP2/ServerThread.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.*;
+
+public class ServerThread extends Thread{
+
+	public DatagramSocket socket = null;
+	public boolean morePlates = true;
+	public Parking park = new Parking();
+
+	public ServerThread(int port,String addrmcast,int portmcast) throws IOException{
+			super("Servidor");
+
+			// abrir socket com port number introduzido
+			socket = new MulticastSocket(port);
+	}
+
+	public void run(){
+
+		System.out.println("Server running cowboy");
+
+		while(morePlates){
+			try{
+				byte[] buf = new byte[256];
+
+				//Receive request
+				DatagramPacket packet = new DatagramPacket(buf,buf.length);
+				socket.receive(packet);
+
+				//Do the job
+				String plateAndOwner = new String(packet.getData());
+
+				System.out.println(plateAndOwner);
+
+				String[] parts = plateAndOwner.split("\\s+");
+
+				String ret;
+				if(parts[0].equals("REGISTER")){
+					ret = this.park.addVehicle(parts[1], parts[2]);
+					buf = ret.getBytes();
+				}else if(parts[0].equals("LOOKUP")){
+					ret = this.park.lookupVehicle(parts[1]);
+					buf = (parts[1] + " " + ret).getBytes();
+				}
+
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buf,buf.length,address,port);
+				socket.send(packet);
+
+			}catch(IOException e){
+				morePlates = false;
+			}
+		}
+		socket.close();
+	}
+}
