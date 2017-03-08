8e2d8a6ae470c93891fd05f6a15b6b4a4c14bce1
gribeirofeup <goncalo.ribeiro@fe.up.pt>
2017-03-08 12:07:03 +0000

    added proj files

---
+++ PROJ/FileChunk.java
@@ -0,0 +1,30 @@
+import java.security.MessageDigest;
+
+public class FileChunk {
+	String fileId;
+	String fileName;
+	int chunkNo;
+	int repDegree;
+	byte[] data;
+	
+	public FileChunk(String filename,byte[] data,int repDegree,int chunkNo){
+		//File name
+		this.fileName = filename;
+		//File data
+		this.data = data;
+		//File chunk no
+		this.chunkNo = chunkNo;
+		//File replication degree
+		this.repDegree = repDegree;
+		//SHA-256 file id
+		MessageDigest md;
+		try{
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(data);
+			this.fileId = md.digest().toString();
+		}catch(Exception e){
+			
+		}
+		
+	}
+}
