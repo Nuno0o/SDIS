fbdad2c5eabfa1b17685fded901f362bfae6bbc7
RuiCS <rui.pedro.csoares@gmail.com>
2017-03-23 15:38:40 +0000

    Packages

---
+++ PROJ/src/threading/Channel.java
@@ -0,0 +1,25 @@
+package threading;
+
+import utilities.Constants;
+import services.Peer;
+import java.net.*;
+
+public class Channel extends Thread {
+	protected Peer peer;
+	protected MulticastSocket msocket;
+	protected byte[] buffer;
+	protected DatagramPacket packet;
+	
+	public Channel(Peer p){
+		super("Peer" + p.peerNumber);
+		
+		this.peer = p;
+		
+		this.msocket = null;
+		
+		this.buffer = new byte[Constants.MAX_BUFFER_SIZE];
+		
+		this.packet = new DatagramPacket(this.buffer,Constants.MAX_BUFFER_SIZE);
+	}
+	
+}
---
+++ PROJ/src/threading/MCchannel.java
@@ -0,0 +1,40 @@
+package threading;
+
+import services.Peer;
+import java.net.*;
+
+public class MCchannel extends Channel {
+	
+	public MCchannel(Peer p){
+		super(p);
+		
+		try{
+			this.msocket = new MulticastSocket(this.peer.portMC);
+			
+			this.msocket.joinGroup(this.peer.mcastMC);
+		}catch(Exception e){
+			System.out.println("Couldn't connect to multicast channel");
+			System.exit(1);
+		}
+	}
+	
+	public void run(){
+		//Channel cycle
+		while(true){
+			//Receive UDP datagram, continue if failed
+			try{
+				this.msocket.receive(this.packet);
+			}catch(Exception e){
+				System.out.println("Error receiving packet on MC");
+				continue;
+			}
+			//Parse packet
+			String packetData = new String(this.packet.getData());
+			String []splitStr = packetData.split("\\s+");
+			
+			
+			
+		}
+	}
+
+}
