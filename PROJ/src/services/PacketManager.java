87cb307138ba07820b0d1273c5f522c0881bfc94
RuiCS <rui.pedro.csoares@gmail.com>
2017-03-24 17:16:56 +0000

    STORED handler Started

--- PROJ/src/services/PacketManager.java
+++ PROJ/src/services/PacketManager.java
@@ -2,25 +2,26 @@ package services;
 
 public class PacketManager {
 	public Peer peer;
-	
+
 	public PacketManager(Peer peer){
 		this.peer = peer;
 	}
-	public void handlePacket(String packet){
+	public boolean handlePacket(String packet){
 		String[] splitStr = packet.split("\\s+");
-		
+
 		switch(splitStr[0]){
 		case "PUTCHUNK":
-			handlePutChunk(splitStr);
-			break;
-		}
-	}
-	
-	public Boolean handlePutChunk(String[] splitStr){
-		if(splitStr[1] != peer.protocol_version){
+			return handlePutChunk(splitStr);
+		default:
 			return false;
 		}
-		if(Integer.parseInt(splitStr[2]) == peer.peerNumber){
+	}
+
+	public boolean handlePutChunk(String[] splitStr){
+		if(splitStr[1] != this.peer.protocol_version){
+			return false;
+		}
+		if(Integer.parseInt(splitStr[2]) == this.peer.peerNumber){
 			return false;
 		}
 		return true;
--- PROJ/src/threading/MDBchannel.java
+++ PROJ/src/threading/MDBchannel.java
@@ -1,6 +1,8 @@
 package threading;
 
+import services.PacketManager;
 import services.Peer;
+
 import java.net.*;
 
 public class MDBchannel extends Channel {
@@ -23,6 +25,15 @@ public class MDBchannel extends Channel {
 			//Receive UDP datagram, continue if failed
 			try{
 				this.msocket.receive(this.packet);
+
+				String packetData = new String(this.packet.getData(), 0, this.packet.getLength());
+
+				// handle STORED packet
+				PacketManager p = new PacketManager(this.peer);
+				if (p.handlePacket(packetData)){
+					// TODO: handler: write chunk to a file
+				}
+
 			}catch(Exception e){
 				System.out.println("Error receiving packet on MC");
 				continue;
@@ -30,7 +41,7 @@ public class MDBchannel extends Channel {
 			//Parse packet
 			String packetData = new String(this.packet.getData());
 			this.packetmanager.handlePacket(packetData);
-			
+
 
 		}
 	}
